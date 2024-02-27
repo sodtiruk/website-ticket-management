94a79832139c20bfaa5d7d6b6c000077fa12a245
sodtiruk <sodtiruk22@gmail.com>
2024-02-28 00:33:00 +0700

    fixed dockerfile and vite.config.js about port

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,17 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
   plugins: [react()],
-  preview: {
-  port: 8080,
-  strictPort: true,
- },
   server: {
-  port: 8080,
-  strictPort: true,
-  host: true,
-  origin: "http://0.0.0.0:8080",
- },
-
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 8080,
+    },
 })
