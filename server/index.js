a504df35b4ddb1738640a863dc1aa1e2447dd3df
sodtiruk <sodtiruk22@gmail.com>
2024-02-28 22:40:47 +0700

    create api get put post

--- server/index.js
+++ server/index.js
@@ -1,39 +1,131 @@
-
 const express = require("express");
 const app = express();
-const port = 3000;
 const mysql = require("mysql");
+const cors = require("cors");
 
+const port = 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({extended : true}));
-
 
 //home
 app.get("/home", (req, res) => {
-
     res.send("hello world")
-
 });
 
 
 app.get("/", (req, res) =>{
     return res.send({
         error: false,
-        message: "Welcome to RESTful CRUD API with NodeJS, Express, MYSQL",
-        written_by: "Sutthirak"
+        message: "Ticket-management with nodejs react",
+        written_by: "Sutthirak Sutsaenya"
     })    
 });    
 
 //connection to database
-// let dbConnection = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "",
-//     database: "todoweb"
-// });    
+let dbConnection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "ticket_management"
+});    
 
-// dbConnection.connect();
+dbConnection.connect();
+
+//get all tickets
+app.get("/tickets", (req, res) => {
+    dbConnection.query("select * from ticket", (error, results, fields) => {
+        if (error) throw error;
+
+        let message = "";
+        if (results == undefined || results.length == 0){
+            message = "ticket empthy";
+        }else {
+            message = "success all tickets";
+        }
+        return res.send({error: false, data: results, message: message})
+    })
+});
+
+//get ticket
+app.get("/ticket/:id", (req, res) => {
+    let id = req.params.id;
+    dbConnection.query("select * from ticket where id = ?", [id], (error, results, fields) => {
+        if (error) throw error;
+
+        let message = "";
+        if (results == undefined || results.length == 0){
+            message = "ticket not found";
+        }else {
+            message = "founded ticket";
+        }
+        return res.send({error: false, data: results, message: message})
+    })
+});
+
+//create ticket
+app.post("/ticket", (req, res) => {
+
+    let title = req.body.title;
+    let description = req.body.description;
+    let contact = req.body.contact;
+    let status = "pending" 
+
+    if (!title || !description || !contact || !status){
+        return res.status(400).send({error: true, message: "Please fill in complete information."})
+    }else {
+        dbConnection.query("insert into ticket (title, description, contact, status) values (?, ?, ?, ?)", [title, description, contact, status], (error, results, fields) => {
+            if (error) throw error;
+            return res.send({error: false, data: results, message: "create ticket successfully"})
+        })
+    }
+})
+
+//update
+app.put("/ticket", (req, res) => {
+    let id = req.body.id;
+    let title = req.body.title;
+    let description = req.body.description;
+    let contact = req.body.contact;
+
+    if (!title || !description || !contact){
+        return res.status(400).send({error: true, message: "Please fill in complete information."})
+    }else {
+        dbConnection.query("update ticket set title = ?, description = ?, contact = ? where id = ?", [title, description, contact, id], (error, results, fields) => {
+            if (error) throw error;
+
+            let message = ""
+
+            if (results.changedRows == 0) {
+                message = "Same ticket or ticket not found"
+            }else {
+                message = "ticket updated"
+            }
+            return res.send({error: false, data: results, message:message})
+        })
+    }
+})
+
+//filter ticket
+app.get("/tickets/filter/:status", (req, res) => {
+    let status = req.params.status;
+    dbConnection.query("select * from ticket where status = ?", [status], (error, results, fields) => {
+        if (error) throw error;
+
+        let message = "";
+        if (results == undefined || results.length == 0){
+            message = "empthy this status.";
+        }else {
+            message = `founded status ${status}`;
+        }
+        return res.send({error: false, data: results, message: message})
+    })
+});
+
+
+
+
 
 
 
